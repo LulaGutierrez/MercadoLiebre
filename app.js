7020d2a8ef6fd7b4cee2487dfc5ad78520e08a78
LulaGutierrez <lujanvicgutierrez@gmail.com>
2022-06-23 23:48:25 -0300

    termine por hoy

--- app.js
+++ app.js
@@ -1,9 +1,8 @@
-const express = require("express");
-const path = require("path");
-const app = express();
-const port = 3030;
+const express = require('express');
+const path = require('path');
+const app = express()
+const port = 3030
 
-const publicPath = path.resolve(__dirname, "./public");
-app.get(express.static(publicPath));
-
-app.listen(port, ()=> console.log("Server running in port " + port));
+app.use(express.static('public'));
+app.listen(port, () => console.log('Server running in http://localhost:' + port))
+app.get('/', (req,res) => res.sendFile(path.join(__dirname, 'views', 'home.html')))
